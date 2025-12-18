b6009911143b2a3500bf358ab1c0c955561bed1e
Saul <thesimulgamer007@gmail.com>
2025-12-18 16:45:23 +0000

    cambios migraciones

--- database/migrations/2025_12_18_144919_deck.php
+++ database/migrations/2025_12_18_144919_deck.php
@@ -4,14 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
+        Schema::create('deck', function (Blueprint $table) {
+            $table->id("deck_id")->unique();
+
+            $table->primary("deck_id");
+            $table->foreignId("user_id")->constrained("user");
+            $table->string("name");
+            $table->boolean("is_public")->default(false);
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('decks');
     }
 };
