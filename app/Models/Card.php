0230c222bcb0e1476dbe7f60378fb6c11eadf407
Josue Castro <114679940+ItsBreo@users.noreply.github.com>
2025-12-18 14:56:56 +0000

    Creación de migraciones

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
-    //
+    // Permitir asignación masiva para todos los campos
+    protected $guarded = [];
+
+    // CASTS: La parte más importante
+    protected $casts = [
+        'data' => 'array',               // <--- LA CLAVE: Convierte JSON DB <-> Array PHP
+        'market_avg_price' => 'decimal:2',
+        'mana_value' => 'float',
+    ];
+
+    public function cardSet()
+    {
+        return $this->belongsTo(CardSet::class);
+    }
 }
---
+++ database/migrations/2025_12_18_144331_card.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+
+            // ID único de Scryfall (UUID) para evitar duplicados
+            $table->string('scryfall_id')->unique();
+
+            // Campos "calientes" (Los sacamos del JSON para buscar rápido con SQL)
+            $table->string('name')->index();
+            $table->string('set_code')->index();
+            $table->string('rarity');
+            $table->decimal('market_avg_price', 10, 2)->default(0);
+            $table->string('image_url')->nullable();
+
+            // === AQUÍ VA EL JSON ENTERO ===
+            // Usamos 'jsonb' para PostgreSQL (Mejor rendimiento que 'json')
+            $table->jsonb('data');
+
+            $table->timestamps();
+
+            // Relación opcional con sets
+            $table->foreignId('card_set_id')->nullable()->constrained('card_sets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
