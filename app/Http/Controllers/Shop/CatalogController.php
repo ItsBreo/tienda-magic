5d0b4af5c0add647b60bdce09a6e7341bbcbaed0
AzaelReyesMartel-1ASIR <azaelrm12@gmail.com>
2025-12-22 20:30:15 +0000

    Defino las funciones a desarrollar en los controladores de carrito y catálogo.

--- app/Http/Controllers/Shop/CatalogController.php
+++ app/Http/Controllers/Shop/CatalogController.php
@@ -1,10 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Shop;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CatalogController extends Controller
+class catalogController extends Controller
 {
-    //
+    /*
+    TODO:
+    Funciones a implementar en catálogo de productos
+
+    - Busqueda avanzada
+    - Filtros
+    - Paginación
+
+    */
 }
--- app/Http/Controllers/Shop/cartController.php
+++ app/Http/Controllers/Shop/cartController.php
@@ -1,10 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Shop;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class cartController extends Controller
 {
-    //
+    /*
+    TODO:
+    Funciones a implementar en carrito
+
+    - Ver el carrito
+    - Agregar un producto al carrito
+    - Eliminar un producto del carrito
+
+    */
 }
