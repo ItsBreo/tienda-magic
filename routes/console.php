680f3e9be59539c409907dcbe99235a64fbc2ae0
Josue <josuemanuelcastrorodriguez2004@gmail.com>
2026-04-16 23:22:10 +0100

    Media del precio de las cartas en mercado secundario

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Registro diario de precios del mercado
+Schedule::command('market:track-prices')->dailyAt('04:00');
+
+// Sincronización maestra con Scryfall (semanal para actualizar precios base)
+Schedule::command('scryfall:sync-master')->weeklyOn(0, '02:00');
